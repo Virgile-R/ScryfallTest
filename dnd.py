29eb0a5e76d3dbb47afb9d2fb69f5c9999c1db35
Virgile-R <virgile.reiter@gmail.com>
2021-07-22 12:58:17 +0200

    essai template

--- dnd.py
+++ dnd.py
@@ -1,3 +1,31 @@
+from django import template
+from django.template import Template, Context
+from django.conf import settings
+from django.shortcuts import render
+from django.template.loader import render_to_string
+
+templates = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ["./html/"],
+    }
+]
+settings.configure(templates)
+
+
+
+
+def my_view(request):
+    as_file = request.GET.get('as_file')
+    t = Template('template.html')
+    c = Context({"name": 'monstre test'} )
+    if as_file:
+        content = render_to_string(t, c)
+        with open('./html/test.html', 'w') as test_file:
+            test_file.write(content)
+
+    return render(t,c)
+my_view('GET')
 class DndTemplate:
     def __init__(self, model, css) -> None:
         pass
